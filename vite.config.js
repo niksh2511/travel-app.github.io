5abc832e26f11a3bc87f2ce592b7ab6a744e7062
niksh2511 <niksh2511@gmail.com>
2024-01-29 12:02:58 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/travel-app.github.io",
+  base: "/travel-app.github.io/",
   plugins: [react()],
 })
